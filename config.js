6505bbc5b3186b952da81f87cc53cb0ae3e6b49e
lleter <107049591+lleter@users.noreply.github.com>
2024-08-07 13:52:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const CLIENT_ID = 'YOUR_CLIENT_ID.apps.googleusercontent.com';
+const CLIENT_ID = '1029230164357-h4fahjnic2ak94g9ovf9nvjlvklo5q5t.apps.googleusercontent.com';
 const API_KEY = 'YOUR_API_KEY';
 const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
 const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
